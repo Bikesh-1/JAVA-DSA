4f21d52554056a526d9d77be04bdd75b6aa7c65a
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-09-02 07:01:35 +0530

    Ading java file

--- src/TwoDarray/ForEach.java
+++ src/TwoDarray/ForEach.java
@@ -1,4 +1,20 @@
 package TwoDarray;
 
 public class ForEach {
+    public static void main(String[] args) {
+        int[][] b = {{1,2,3},{4,5,6}};
+        for (int[] ele : b){
+            for (int x :ele){
+                System.out.print(x+" ");
+            }
+            System.out.println();
+        }
+//        for (int i = 0; i < 2; i++) {
+//            for (int j = 0; j < 3; j++) {
+//                System.out.println(b[i][j]+" ");
+//            }
+//
+//        }
+
+    }
 }
