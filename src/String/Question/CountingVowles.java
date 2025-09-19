3c5dde475217c023aa3d4787a5305e911c7efd43
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-09-20 01:03:12 +0530

    Ading java file

--- src/String/Question/CountingVowles.java
+++ src/String/Question/CountingVowles.java
@@ -1,4 +1,16 @@
 package String.Question;
-
 public class CountingVowles {
+    public static void main(String[] args) {
+        String s = "Bikesh Kumar";
+        int n = s.length();
+        int countVowels = 0;
+        for (int i = 0; i <n; i++) {
+            char ch = s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+                countVowels++;
+            }
+        }
+        System.out.println(countVowels);
+
+    }
 }
